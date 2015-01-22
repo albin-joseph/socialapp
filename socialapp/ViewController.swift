18148dee95a2c87b3618d44c13e5baa922b38208
ALBIN JOSEPH <albinjoseph007@gmail.com>
2015-01-22 13:40:56 +0530

    Social App initaial commit
    changes

--- socialapp/ViewController.swift
+++ socialapp/ViewController.swift
@@ -13,6 +13,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let button   = UIButton.buttonWithType(UIButtonType.System) as UIButton
+        button.frame = CGRectMake(100, 100, 100, 50)
+        button.backgroundColor = UIColor.lightGrayColor()
+        button.layer.cornerRadius=8.0;
+        button.setTitle("Test Button", forState: UIControlState.Normal)
+        button.addTarget(self, action: "buttonAction:", forControlEvents: UIControlEvents.TouchUpInside)
+        
+        self.view.addSubview(button)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+
+    func buttonAction(sender:UIButton!)
+    {
+        
+        
+        var alert = UIAlertController(title: "Alert", message: "Button tapped", preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
 
 }
 
