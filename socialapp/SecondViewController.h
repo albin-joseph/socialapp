f260e7e9fcaba13b202a6d051bf2870aee4aac91
albin-joseph <albinjoseph007@gmail.com>
2015-01-27 22:24:02 +0530

    add new

---
+++ socialapp/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  socialapp
+//
+//  Created by Albin Joseph on 27/01/15.
+//  Copyright (c) 2015 RapidValueSolutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
